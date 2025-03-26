cc538148a3c4f4d35880a3a5eb954f139527d120
Alexandre Bonhomme <alexandre.bonhomme@lilo.org>
2025-03-26 09:29:48 +0100

    doc: adds schematic and doc

--- midi_cv_v2/midi_cv_v2.ino
+++ midi_cv_v2/midi_cv_v2.ino
@@ -1,19 +1,16 @@
-/*
-
- */
-
 #include <MIDI.h>
 #include <Adafruit_MCP4728.h>
 #include <Wire.h>
 
-#define GATE 8
+#define GATE_1 8
 #define SLIDE 9
 #define ACCENT 10
-// #define GATE_2 11
+#define GATE_2 11
+
 #define CLOCK 12
 
-#define MIDI_CH1 1
-#define MIDI_CH2 2
+#define MIDI_CH1 4
+#define MIDI_CH2 9
 
 #define CC_1 70
 #define CC_2 71
@@ -37,17 +34,26 @@ const PROGMEM long cv[121] = {
     3310, 3344, 3378, 3413, 3447, 3481, 3515, 3549, 3583, 3617, 3651, 3686,
     3720, 3754, 3788, 3822, 3856, 3890, 3924, 3959, 3993, 4027, 4061, 4095};
 
+bool clock_on = false;
+int clock_count = 1;
+int dac_channel, note_count_ch1 = 0;
+
+/**
+ *
+ */
 void setup()
 {
   pinMode(CLOCK, OUTPUT);
-  pinMode(GATE, OUTPUT);
+  pinMode(GATE_1, OUTPUT);
   pinMode(SLIDE, OUTPUT);
   pinMode(ACCENT, OUTPUT);
+  pinMode(GATE_2, OUTPUT);
 
   digitalWrite(CLOCK, LOW);
-  digitalWrite(GATE, LOW);
+  digitalWrite(GATE_1, LOW);
   digitalWrite(SLIDE, LOW);
   digitalWrite(ACCENT, LOW);
+  digitalWrite(GATE_2, LOW);
 
   mcp.begin();
 
@@ -60,20 +66,143 @@ void setup()
   MIDI.begin(MIDI_CHANNEL_OMNI);
 }
 
-static int clock_count = 1;
-int dac_channel, note_count = 0;
-
+/**
+ *
+ */
 void loop()
 {
   MIDI.read();
 }
 
-int isActiveChannel(byte channel)
+/**
+ *
+ */
+void handleNoteOn(byte channel, byte pitch, byte velocity)
 {
-  // Only 2 channels support
-  return (MIDI_CH1 == channel || MIDI_CH2 == channel);
+  if (velocity == 0)
+  {
+    // This acts like a NoteOff.
+    handleNoteOff(channel, pitch);
+    return;
+  }
+
+  if (channel == MIDI_CH1)
+  {
+    note_count_ch1++;
+
+    // CV
+    commandNote(channel, pitch);
+
+    // Gate
+    digitalWrite(GATE_1, HIGH);
+
+    // Accent
+    digitalWrite(ACCENT, velocity > 80 ? HIGH : LOW);
+
+    // Slide
+    if (note_count_ch1 > 1)
+    {
+      digitalWrite(SLIDE, HIGH);
+    }
+  }
+
+  if (channel == MIDI_CH2)
+  {
+    // CV
+    commandNote(channel, pitch);
+
+    // Gate
+    digitalWrite(GATE_2, HIGH);
+  }
 }
 
+/**
+ *
+ */
+void handleNoteOff(byte channel, byte pitch)
+{
+  if (channel == MIDI_CH1)
+  {
+    note_count_ch1--;
+
+    if (note_count_ch1 < 0)
+    {
+      note_count_ch1 = 0;
+    }
+
+    if (note_count_ch1 == 0)
+    {
+      // Gate
+      digitalWrite(GATE_1, LOW);
+
+      // Accent
+      digitalWrite(ACCENT, LOW);
+
+      // Slide
+      digitalWrite(SLIDE, LOW);
+    }
+  }
+
+  if (channel == MIDI_CH2)
+  {
+    // Gate
+    digitalWrite(GATE_2, LOW);
+  }
+}
+
+/**
+ *
+ */
+void handleClock(void)
+{
+  // Clock out everytime receives clock in normal mode
+  if (clock_count < PPQN_CLOCK)
+  {
+    clock_count++;
+
+    if (clock_on)
+    {
+      clock_on = false;
+
+      digitalWrite(CLOCK, LOW);
+    }
+  }
+  else
+  {
+    clock_count = 1;
+    clock_on = true;
+
+    digitalWrite(CLOCK, HIGH);
+  }
+}
+
+/**
+ *
+ */
+void handleControlChange(byte channel, byte number, byte value)
+{
+  if (!isActiveChannel(channel))
+  {
+    return;
+  }
+
+  if (number == CC_1 || number == CC_2)
+  {
+    commandCC(number, value);
+  }
+}
+
+/**
+ *
+ */
+bool isActiveChannel(byte channel)
+{
+  return channel == MIDI_CH1 || channel == MIDI_CH2;
+}
+
+/**
+ *
+ */
 byte processNote(byte note)
 {
   int val = note - 12; // C0 = 12
@@ -91,114 +220,12 @@ byte processNote(byte note)
   return val;
 }
 
-// @todo handle channel 2 (gate, etc.)
-void handleNoteOn(byte channel, byte pitch, byte velocity)
-{
-  if (!isActiveChannel(channel))
-  {
-    return;
-  }
-
-  if (velocity == 0)
-  {
-    // This acts like a NoteOff.
-    handleNoteOff(channel, pitch, velocity);
-    return;
-  }
-
-  byte note = processNote(pitch);
-
-  note_count++;
-
-  // CV
-  commandNote(channel, note);
-
-  // Gate
-  digitalWrite(GATE, HIGH);
-
-  // Accent
-  if (velocity > 80)
-  {
-    digitalWrite(ACCENT, HIGH);
-  }
-  else
-  {
-    digitalWrite(ACCENT, LOW);
-  }
-
-  // Slide
-  if (note_count > 1)
-  {
-    digitalWrite(SLIDE, HIGH);
-  }
-}
-
-// @todo handle channel 2 (gate, etc.)
-void handleNoteOff(byte channel, byte pitch, byte velocity)
-{
-  if (!isActiveChannel(channel))
-  {
-    return;
-  }
-
-  byte note = processNote(pitch);
-
-  note_count--;
-
-  if (note_count < 0)
-  {
-    note_count = 0;
-  }
-
-  if (note_count == 0)
-  {
-    // Gate
-    digitalWrite(GATE, LOW);
-
-    // Accent
-    digitalWrite(ACCENT, LOW);
-
-    // Slide
-    digitalWrite(SLIDE, LOW);
-  }
-}
-
-void handleClock(void)
-{
-  // Clock out everytime receives clock in normal mode
-  if (clock_count < PPQN_CLOCK)
-  {
-    clock_count++;
-
-    digitalWrite(CLOCK, LOW);
-  }
-  else
-  {
-    clock_count = 1;
-
-    // Send trigger to CLOCK port
-    digitalWrite(CLOCK, HIGH);
-  }
-}
-
-void handleControlChange(byte channel, byte number, byte value)
-{
-  if (!isActiveChannel(channel))
-  {
-    return;
-  }
-
-  if (number == CC_1 || number == CC_2)
-  {
-    commandCC(number, value);
-  }
-}
-
 /**
  * @brief Send CV to DAC
  */
-void commandNote(byte channel, byte note)
+void commandNote(byte channel, byte pitch)
 {
+  byte note = processNote(pitch);
   uint16_t mV = pgm_read_dword_near(cv + note);
 
   mcp.setChannelValue(channel == MIDI_CH1 ? MCP4728_CHANNEL_C : MCP4728_CHANNEL_D, mV, MCP4728_VREF_INTERNAL, MCP4728_GAIN_2X);
